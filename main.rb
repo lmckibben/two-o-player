3422506fbef139a93a4c9c293dcd57aeeb964a72
Leland Mckibben <lmckibben28@hotmail.com>
2021-10-11 21:26:26 +0000

    started game loop

--- main.rb
+++ main.rb
@@ -8,3 +8,7 @@ player2 = Player.new("Player Two")
 game = Game.new([player1, player2])
 
 puts "Let the games begin! #{game.current_player.name} your turn frist"
+
+while !game.game_over do
+  question = Question.new(game.next_round)
+end
