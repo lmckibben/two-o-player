2bdd3ed1b0367aed805e1d2b94bc1055707ab677
Leland Mckibben <lmckibben28@hotmail.com>
2021-10-11 21:11:38 +0000

    initialized game class, added first message

--- main.rb
+++ main.rb
@@ -1,5 +1,10 @@
 require_relative './player'
 require_relative './question'
+require_relative './game'
 
-player1 = Player.new()
-player2 = Player.new()
+player1 = Player.new("Player One")
+player2 = Player.new("Player Two")
+
+game = Game.new([player1, player2])
+
+puts "Let the games begin! #{game.current_player.name} your turn frist"
